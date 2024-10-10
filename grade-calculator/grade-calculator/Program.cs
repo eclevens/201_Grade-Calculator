8f397546f316e838100c812226977269fc0c0e47
eclevens <ellaclevens@gmail.com>
2024-10-10 09:31:26 -0400

    Initial commit

---
+++ grade-calculator/grade-calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿/*  Ella Clevens
+ *  IGME 201
+    10/13/24*/
+namespace grade_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
