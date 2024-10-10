0941e2f6a76903ec0ea00caf23b9c3a77173ad11
eclevens <ellaclevens@gmail.com>
2024-10-10 19:52:59 -0400

    Completed initial steps through loop array

--- grade-calculator/grade-calculator/Program.cs
+++ grade-calculator/grade-calculator/Program.cs
@@ -7,7 +7,18 @@ namespace grade_calculator
     {
         static void Main(string[] args)
         {
-            
+            //Variables
+            //String variable to hold name of student (me)
+            string studentName = "Ella";
+            //Create int array of 10 numbers
+            int[] grades = { 100, 92, 87, 200, -20, 52, 82, 75, 67, 88 };
+
+            //program start
+            Console.WriteLine("Welcome "+ studentName);
+            Console.WriteLine("Here are your grades: ");
+
+            //Loop through grades array
+
         }
     }
 }
